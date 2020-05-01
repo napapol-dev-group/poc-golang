33822e26757a59bce736ddb640bcbcb0d37e2507
WasinTaw <dreamwstt@gmail.com>
2020-05-01 22:24:12 +0700

    Add log

--- app/handlers/delivery/http/register.go
+++ app/handlers/delivery/http/register.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/napapol-dev-group/poc-golang/app/handlers/delivery/http/models"
 	"github.com/napapol-dev-group/poc-golang/app/utils"
@@ -12,12 +13,14 @@ func (h *handler) Register(c *gin.Context) {
 	var requestData models.RegisterUserRequest
 	err := c.ShouldBindJSON(&requestData)
 	if err != nil {
+		fmt.Println(err)
 		c.AbortWithStatusJSON(http.StatusBadRequest, err)
 		return
 	}
 
 	createdUser, err := h.UserUseCase.RegisterUser(requestData.Entity())
 	if err != nil {
+		fmt.Println(err)
 		c.AbortWithStatusJSON(http.StatusBadRequest, err)
 		return
 	}
